409cb889c106c77d4647fc14156f594d3ab383a9
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-10-13 16:33:08 -0300

    feat: implementing application layer services and testing controllers

---
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+using CleanArchMvc.Application.Interfaces;
+
+using Microsoft.AspNetCore.Mvc; 
+
+namespace CleanArchMvc.WebUI.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly IProductService _service;
+    public ProductsController(IProductService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var products = await _service.GetAllAsync(); 
+        return View(products);
+    }
+}
